a9e53bbd25e206536ea875df3c19fb995d71eafe
Tay8472 <tpm8472@gmail.com>
2020-09-21 19:39:35 -0600

    some modifications

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 let express = require("express");
+let cors = require("cors");
+let helmet = require("helmet");
+
 let cookieParser = require("cookie-parser");
 let userRouter = require("../database/potluck/routers/user-router");
 let guestRouter = require("../database/guests/routers/guest-router");
 
 let server = express();
+
+server.use(helmet());
+server.use(cors());
 
 server.use(express.json());
 server.use(cookieParser());
