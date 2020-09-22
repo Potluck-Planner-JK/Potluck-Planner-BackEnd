e2df39d816cd6543934e71ef266c808c6a19295a
Tay8472 <tpm8472@gmail.com>
2020-09-21 19:22:01 -0600

    npx knex init

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,6 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
       filename: "./database/data.db3",
@@ -20,23 +17,8 @@ module.exports = {
       },
     },
   },
-
-  staging: {
-    client: 'sqlite3',
-    connection: {
-      filename: "./database/data.db3",
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
       filename: "./database/data.db3",
@@ -52,5 +34,5 @@ module.exports = {
         conn.run("PRAGMA foreign_keys = ON", done);
       },
     },
-  }
+  },
 };
